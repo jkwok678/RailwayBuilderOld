26ba82449a9a13dd4c88c4e99e6153cfa62ee8e3
jkwok678 <jkwok678@gmail.com>
2020-04-19 00:33:46 +0100

    Added 1st line of trackButtons of block1

--- window.cpp
+++ window.cpp
@@ -129,37 +129,171 @@ void Window::createTrackMenu()
 
 void Window::chooseStraightH()
 {
-    chosen = ElementType::STRAIGHTH;
+   if (chosen!=ElementType::STRAIGHTH)
+        chosen = ElementType::STRAIGHTH;
+   else
+       chosen = ElementType::NONE;
+
 }
 
 void Window::chooseStraightV()
 {
-    chosen = ElementType::STRAIGHTV;
+    if (chosen!=ElementType::STRAIGHTV)
+         chosen = ElementType::STRAIGHTV;
+    else
+        chosen = ElementType::NONE;
 }
 
 void Window::chooseDirectLeft()
 {
-
+    if (chosen!=ElementType::DIRECTLEFT)
+         chosen = ElementType::DIRECTLEFT;
+    else
+        chosen = ElementType::NONE;
 }
 
 void Window::chooseDirectRight()
 {
-
+    if (chosen!=ElementType::DIRECTRIGHT)
+         chosen = ElementType::DIRECTRIGHT;
+    else
+        chosen = ElementType::NONE;
 }
 
 void Window::chooseDirectUp()
 {
-
+    if (chosen!=ElementType::DIRECTUP)
+         chosen = ElementType::DIRECTUP;
+    else
+        chosen = ElementType::NONE;
 }
 
 void Window::chooseDirectDown()
 {
-
+    if (chosen!=ElementType::DIRECTDOWN)
+         chosen = ElementType::DIRECTDOWN;
+    else
+        chosen = ElementType::NONE;
 }
+
+void Window::chooseStraightLeftUp()
+{
+    if (chosen!=ElementType::STRAIGHTLEFTUP)
+         chosen = ElementType::STRAIGHTLEFTUP;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseStraightRightUp()
+{
+    if (chosen!=ElementType::STRIAGHTRIGHTUP)
+         chosen = ElementType::STRIAGHTRIGHTUP;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseDirectLeftUp()
+{
+    if (chosen!=ElementType::DIRECTLEFTUP)
+         chosen = ElementType::DIRECTLEFTUP;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseDirectRightUp()
+{
+    if (chosen!=ElementType::DIRECTRIGHTUP)
+         chosen = ElementType::DIRECTRIGHTUP;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseDirectLeftDown()
+{
+    if (chosen!=ElementType::DIRECTLEFTDOWN)
+         chosen = ElementType::DIRECTLEFTDOWN;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseDirectRightDown()
+{
+    if (chosen!=ElementType::DIRECTRIGHTDOWN)
+         chosen = ElementType::DIRECTRIGHTDOWN;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseTightCurve1()
+{
+    if (chosen!=ElementType::TIGHTCURVE1)
+         chosen = ElementType::TIGHTCURVE1;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseTightCurve2()
+{
+    if (chosen!=ElementType::TIGHTCURVE2)
+         chosen = ElementType::TIGHTCURVE2;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseTightCurve3()
+{
+    if (chosen!=ElementType::TIGHTCURVE3)
+         chosen = ElementType::TIGHTCURVE3;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseTightCurve4()
+{
+    if (chosen!=ElementType::TIGHTCURVE4)
+         chosen = ElementType::TIGHTCURVE4;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseCurve1()
+{
+    if (chosen!=ElementType::CURVE1)
+         chosen = ElementType::CURVE1;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseCurve2()
+{
+    if (chosen!=ElementType::CURVE2)
+         chosen = ElementType::CURVE2;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseCurve3()
+{
+    if (chosen!=ElementType::CURVE3)
+         chosen = ElementType::CURVE3;
+    else
+        chosen = ElementType::NONE;
+}
+
+void Window::chooseCurve4()
+{
+    if (chosen!=ElementType::CURVE4)
+         chosen = ElementType::CURVE4;
+    else
+        chosen = ElementType::NONE;
+}
+
+
 
 void Window::createTrackBlock1()
 {
     trackBlock1 = new QGridLayout;
+
     straightHButton = new QToolButton();
     straightHButton->setMaximumSize(QSize(32,32));
     chooseStraightHAct = new QAction();
@@ -188,9 +322,141 @@ void Window::createTrackBlock1()
     directRightButton->setMaximumSize(QSize(32,32));
     chooseDirectRightAct = new QAction();
     connect(chooseDirectRightAct, &QAction::triggered, this, &Window::chooseDirectRight);
-    directRightIcon = new QIcon(":/graphics/graphics/straightV.png");
+    directRightIcon = new QIcon(":/graphics/graphics/directRight.png");
     directRightButton->setIcon(*directRightIcon);
-    trackBlock1->addWidget(directRightButton,0,1);
+    trackBlock1->addWidget(directRightButton,0,3);
 
+    directUpButton = new QToolButton();
+    directUpButton->setMaximumSize(QSize(32,32));
+    chooseDirectUpAct = new QAction();
+    connect(chooseDirectUpAct, &QAction::triggered, this, &Window::chooseDirectUp);
+    directUpIcon = new QIcon(":/graphics/graphics/directUp.png");
+    directUpButton->setIcon(*directUpIcon);
+    trackBlock1->addWidget(directUpButton,0,4);
+
+    directDownButton = new QToolButton();
+    directDownButton->setMaximumSize(QSize(32,32));
+    chooseDirectDownAct = new QAction();
+    connect(chooseDirectDownAct, &QAction::triggered, this, &Window::chooseDirectDown);
+    directDownIcon = new QIcon(":/graphics/graphics/directDown.png");
+    directDownButton->setIcon(*directDownIcon);
+    trackBlock1->addWidget(directDownButton,0,5);
+
+    straightRightUpButton = new QToolButton();
+    straightRightUpButton->setMaximumSize(QSize(32,32));
+    chooseStraightRightUpAct = new QAction();
+    connect(chooseStraightRightUpAct, &QAction::triggered, this, &Window::chooseStraightRightUp);
+    straightRightUpIcon = new QIcon(":/graphics/graphics/straightRightUp.png");
+    straightRightUpButton->setIcon(*straightRightUpIcon);
+    trackBlock1->addWidget(straightRightUpButton,0,6);
+
+    straightLeftUpButton = new QToolButton();
+    straightLeftUpButton->setMaximumSize(QSize(32,32));
+    chooseStraightLeftUpAct = new QAction();
+    connect(chooseStraightLeftUpAct, &QAction::triggered, this, &Window::chooseStraightLeftUp);
+    straightLeftUpIcon = new QIcon(":/graphics/graphics/straightLeftUp.png");
+    straightLeftUpButton->setIcon(*straightLeftUpIcon);
+    trackBlock1->addWidget(straightLeftUpButton,0,7);
+
+    directRightUpButton = new QToolButton();
+    directRightUpButton->setMaximumSize(QSize(32,32));
+    chooseDirectRightUpAct = new QAction();
+    connect(chooseDirectRightUpAct, &QAction::triggered, this, &Window::chooseDirectRightUp);
+    directRightUpIcon = new QIcon(":/graphics/graphics/directRightUp.png");
+    directRightUpButton->setIcon(*directRightUpIcon);
+    trackBlock1->addWidget(directRightUpButton,0,8);
+
+    directLeftUpButton = new QToolButton();
+    directLeftUpButton->setMaximumSize(QSize(32,32));
+    chooseDirectLeftUpAct = new QAction();
+    connect(chooseDirectLeftUpAct, &QAction::triggered, this, &Window::chooseDirectLeftUp);
+    directLeftUpIcon = new QIcon(":/graphics/graphics/directLeftUp.png");
+    directLeftUpButton->setIcon(*directLeftUpIcon);
+    trackBlock1->addWidget(directLeftUpButton,0,9);
+
+    directLeftDownButton = new QToolButton();
+    directLeftDownButton->setMaximumSize(QSize(32,32));
+    chooseDirectLeftDownAct = new QAction();
+    connect(chooseDirectLeftDownAct, &QAction::triggered, this, &Window::chooseDirectLeftDown);
+    directLeftDownIcon = new QIcon(":/graphics/graphics/directLeftDown.png");
+    directLeftDownButton->setIcon(*directLeftDownIcon);
+    trackBlock1->addWidget(directLeftDownButton,0,10);
+
+    directRightDownButton = new QToolButton();
+    directRightDownButton->setMaximumSize(QSize(32,32));
+    chooseDirectRightDownAct = new QAction();
+    connect(chooseDirectRightDownAct, &QAction::triggered, this, &Window::chooseDirectRightDown);
+    directRightDownIcon = new QIcon(":/graphics/graphics/directRightDown.png");
+    directRightDownButton->setIcon(*directRightDownIcon);
+    trackBlock1->addWidget(directRightDownButton,0,11);
+
+    tightCurve1Button = new QToolButton();
+    tightCurve1Button->setMaximumSize(QSize(32,32));
+    chooseTightCurve1Act = new QAction();
+    connect(chooseTightCurve1Act, &QAction::triggered, this, &Window::chooseTightCurve1);
+    tightCurve1Icon = new QIcon(":/graphics/graphics/tightCurve1.png");
+    tightCurve1Button->setIcon(*tightCurve1Icon);
+    trackBlock1->addWidget(tightCurve1Button,0,12);
+
+
+    tightCurve2Button = new QToolButton();
+    tightCurve2Button->setMaximumSize(QSize(32,32));
+    chooseTightCurve2Act = new QAction();
+    connect(chooseTightCurve2Act, &QAction::triggered, this, &Window::chooseTightCurve2);
+    tightCurve2Icon = new QIcon(":/graphics/graphics/tightCurve2.png");
+    tightCurve2Button->setIcon(*tightCurve2Icon);
+    trackBlock1->addWidget(tightCurve2Button,0,13);
+
+
+    tightCurve3Button = new QToolButton();
+    tightCurve3Button->setMaximumSize(QSize(32,32));
+    chooseTightCurve3Act = new QAction();
+    connect(chooseTightCurve3Act, &QAction::triggered, this, &Window::chooseTightCurve3);
+    tightCurve3Icon = new QIcon(":/graphics/graphics/tightCurve3.png");
+    tightCurve3Button->setIcon(*tightCurve3Icon);
+    trackBlock1->addWidget(tightCurve3Button,0,14);
+
+
+    tightcurve4Button = new QToolButton();
+    tightcurve4Button->setMaximumSize(QSize(32,32));
+    chooseTightcurve4Act = new QAction();
+    connect(chooseTightcurve4Act, &QAction::triggered, this, &Window::chooseTightCurve4);
+    tightcurve4Icon = new QIcon(":/graphics/graphics/tightcurve4.png");
+    tightcurve4Button->setIcon(*tightcurve4Icon);
+    trackBlock1->addWidget(tightcurve4Button,0,15);
+
+    curve1Button = new QToolButton();
+    curve1Button->setMaximumSize(QSize(32,32));
+    chooseCurve1Act = new QAction();
+    connect(chooseCurve1Act, &QAction::triggered, this, &Window::chooseCurve1);
+    curve1Icon = new QIcon(":/graphics/graphics/curve1.png");
+    curve1Button->setIcon(*curve1Icon);
+    trackBlock1->addWidget(curve1Button,0,16);
+
+
+    curve2Button = new QToolButton();
+    curve2Button->setMaximumSize(QSize(32,32));
+    chooseCurve2Act = new QAction();
+    connect(chooseCurve2Act, &QAction::triggered, this, &Window::chooseCurve2);
+    curve2Icon = new QIcon(":/graphics/graphics/curve2.png");
+    curve2Button->setIcon(*curve2Icon);
+    trackBlock1->addWidget(curve2Button,0,17);
+
+    curve3Button = new QToolButton();
+    curve3Button->setMaximumSize(QSize(32,32));
+    chooseCurve3Act = new QAction();
+    connect(chooseCurve3Act, &QAction::triggered, this, &Window::chooseCurve3);
+    curve3Icon = new QIcon(":/graphics/graphics/curve3.png");
+    curve3Button->setIcon(*curve3Icon);
+    trackBlock1->addWidget(curve3Button,0,18);
+
+
+    curve4Button = new QToolButton();
+    curve4Button->setMaximumSize(QSize(32,32));
+    chooseCurve4Act = new QAction();
+    connect(chooseCurve4Act, &QAction::triggered, this, &Window::chooseCurve4);
+    curve4Icon = new QIcon(":/graphics/graphics/curve4.png");
+    curve4Button->setIcon(*curve4Icon);
+    trackBlock1->addWidget(curve4Button,0,19);
 
 }
--- window.h
+++ window.h
@@ -32,6 +32,20 @@ private slots:
     void chooseDirectRight();
     void chooseDirectUp();
     void chooseDirectDown();
+    void chooseStraightLeftUp();
+    void chooseStraightRightUp();
+    void chooseDirectLeftUp();
+    void chooseDirectRightUp();
+    void chooseDirectLeftDown();
+    void chooseDirectRightDown();
+    void chooseTightCurve1();
+    void chooseTightCurve2();
+    void chooseTightCurve3();
+    void chooseTightCurve4();
+    void chooseCurve1();
+    void chooseCurve2();
+    void chooseCurve3();
+    void chooseCurve4();
 
 private:
     Map currentLayout;
@@ -98,9 +112,75 @@ private:
     QAction *chooseDirectDownAct;
     QIcon *directDownIcon;
 
+    //StraightLeftUpButton
+    QToolButton *straightLeftUpButton;
+    QAction *chooseStraightLeftUpAct;
+    QIcon *straightLeftUpIcon;
 
+    //StraightRightUpButton
+    QToolButton *straightRightUpButton;
+    QAction *chooseStraightRightUpAct;
+    QIcon *straightRightUpIcon;
 
+    //DirectLeftUpButton
+    QToolButton *directLeftUpButton;
+    QAction *chooseDirectLeftUpAct;
+    QIcon *directLeftUpIcon;
 
+    //DirectRightUpButton
+    QToolButton *directRightUpButton;
+    QAction *chooseDirectRightUpAct;
+    QIcon *directRightUpIcon;
+
+    //DirectLeftDownButton
+    QToolButton *directLeftDownButton;
+    QAction *chooseDirectLeftDownAct;
+    QIcon *directLeftDownIcon;
+
+    //DirectRightDownButton
+    QToolButton *directRightDownButton;
+    QAction *chooseDirectRightDownAct;
+    QIcon *directRightDownIcon;
+
+    //TightCurve1Button
+    QToolButton *tightCurve1Button;
+    QAction *chooseTightCurve1Act;
+    QIcon *tightCurve1Icon;
+
+    //TightCurve2Button
+    QToolButton *tightCurve2Button;
+    QAction *chooseTightCurve2Act;
+    QIcon *tightCurve2Icon;
+
+    //TightCurve3Button
+    QToolButton *tightCurve3Button;
+    QAction *chooseTightCurve3Act;
+    QIcon *tightCurve3Icon;
+
+    //Tightcurve4Button
+    QToolButton *tightcurve4Button;
+    QAction *chooseTightcurve4Act;
+    QIcon *tightcurve4Icon;
+
+    //Curve1Button
+    QToolButton *curve1Button;
+    QAction *chooseCurve1Act;
+    QIcon *curve1Icon;
+
+    //Curve2Button
+    QToolButton *curve2Button;
+    QAction *chooseCurve2Act;
+    QIcon *curve2Icon;
+
+    //Curve3Button
+    QToolButton *curve3Button;
+    QAction *chooseCurve3Act;
+    QIcon *curve3Icon;
+
+    //Curve4Button
+    QToolButton *curve4Button;
+    QAction *chooseCurve4Act;
+    QIcon *curve4Icon;
 };
 
 #endif // WINDOW_H
